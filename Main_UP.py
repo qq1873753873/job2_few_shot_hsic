54bf5e7021288c94cf2fd515574f771a3fc7ea22
Ryoma <feizhu0826@foxmail.com>
2024-12-20 11:28:16 +0800

    Add files via upload

---
+++ Main_UP.py
@@ -0,0 +1,327 @@
+import time
+import torch
+import random
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn import preprocessing
+from Function import load_data, generate, LDA_SLIC, GreedyTrainingStrategy
+from Network import GS_GraphSAT
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+samples_type = ['ratio', 'same_num'][0]
+
+dataset = 'UP'
+Dataset = dataset.upper()
+data, gt = load_data.load_dataset(Dataset)
+
+# Seed_List = [1, 2, 3, 4, 5]
+Seed_List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+gt_hsi = gt.reshape(np.prod(gt.shape[:2]),)
+class_count = max(gt_hsi)
+print('The class numbers of the HSI data is:', class_count)
+
+print('-----Importing Setting Parameters----')
+learning_rate, max_epoch, curr_train_ratio, val_ratio, Scale = 0.0005, 500, 0.003, 0.007, 500  # up 0.5%
+# learning_rate, max_epoch, curr_train_ratio, val_ratio, Scale = 0.0005, 500, 0.005, 0.005, 500  # up 0.5%
+
+dataset_name = Dataset
+torch.cuda.empty_cache()
+OA_ALL = []
+AA_ALL = []
+KPP_ALL = []
+AVG_ALL = []
+Train_Time_ALL = []
+Test_Time_ALL = []
+
+superpixel_scale = Scale
+train_samples_per_class = curr_train_ratio
+val_samples = class_count
+train_ratio = curr_train_ratio
+cmap = plt.get_cmap('jet', class_count + 1)
+plt.set_cmap(cmap)
+m, n, d = data.shape
+
+orig_data = data
+
+height, width, bands = data.shape
+data = np.reshape(data, [height * width, bands])
+minMax = preprocessing.StandardScaler()
+data = minMax.fit_transform(data)
+data = np.reshape(data, [height, width, bands])
+
+for curr_seed in Seed_List:
+
+    print('train_inter:', curr_seed)
+    random.seed(curr_seed)
+    gt_reshape = np.reshape(gt, [-1])  # (21025,)
+    train_rand_idx = []
+    val_rand_idx = []
+    test_data_index, train_data_index, val_data_index = generate.samples(samples_type, class_count, train_ratio,
+                                                                    val_ratio, train_samples_per_class, val_samples, gt)
+
+    Test_GT, train_samples_gt, test_samples_gt, val_samples_gt, train_samples_gt_onehot,\
+    test_samples_gt_onehot, val_samples_gt_onehot, train_label_mask, test_label_mask, \
+    val_label_mask = generate.index_samples(height, width, class_count, test_data_index, train_data_index, val_data_index, gt)
+
+    ls = LDA_SLIC.LDA_SLIC(data, np.reshape(train_samples_gt, [height, width]), class_count - 1)
+    tic0 = time.time()
+    Q, S, A, Seg = ls.simple_superpixel(scale=superpixel_scale)
+
+    toc0 = time.time()
+    LDA_SLIC_Time = toc0 - tic0
+
+    print("LDA-SLIC costs time: {}".format(LDA_SLIC_Time))
+    print('Q', Q.shape)
+    Q = torch.from_numpy(Q).to(device)
+    A = torch.from_numpy(A).to(device)
+
+    train_samples_gt = torch.from_numpy(train_samples_gt.astype(np.float32)).to(device)
+    test_samples_gt = torch.from_numpy(test_samples_gt.astype(np.float32)).to(device)
+    val_samples_gt = torch.from_numpy(val_samples_gt.astype(np.float32)).to(device)
+
+    train_samples_gt_onehot = torch.from_numpy(train_samples_gt_onehot.astype(np.float32)).to(device)
+    test_samples_gt_onehot = torch.from_numpy(test_samples_gt_onehot.astype(np.float32)).to(device)
+    val_samples_gt_onehot = torch.from_numpy(val_samples_gt_onehot.astype(np.float32)).to(device)
+
+    train_label_mask = torch.from_numpy(train_label_mask.astype(np.float32)).to(device)
+    test_label_mask = torch.from_numpy(test_label_mask.astype(np.float32)).to(device)
+    val_label_mask = torch.from_numpy(val_label_mask.astype(np.float32)).to(device)
+
+    net_input = np.array(data, np.float32)
+    net_input = torch.from_numpy(net_input.astype(np.float32)).to(device)
+
+    net = GS_GraphSAT(height, width, net_input.shape[2], class_count, Q, A).to(device)
+
+    # 参数量
+    # print("parameters", net.parameters(), len(list(net.parameters())))
+    # total = sum([param.nelement() for param in net.parameters()])
+    # print('parameters:', total)
+    # from thop import profile
+    # flops, params = profile(net, inputs=(net_input, ))
+    # print('flops:', flops)
+
+    def compute_loss(predict: torch.Tensor, reallabel_onehot: torch.Tensor, reallabel_mask: torch.Tensor):
+        real_labels = reallabel_onehot
+        we = -torch.mul(real_labels, torch.log(predict))
+        we = torch.mul(we, reallabel_mask)
+        pool_cross_entropy = torch.sum(we)
+        return pool_cross_entropy
+
+    zeros = torch.zeros([m * n]).to(device).float()
+
+    def evaluate_performance(network_output, train_samples_gt, train_samples_gt_onehot, require_AA_KPP=False,
+                                printFlag=True):
+        if False == require_AA_KPP:
+            with torch.no_grad():
+                available_label_idx = (train_samples_gt != 0).float()
+                available_label_count = available_label_idx.sum()
+                correct_prediction = torch.where(
+                    torch.argmax(network_output, 1) == torch.argmax(train_samples_gt_onehot, 1), available_label_idx,
+                    zeros).sum()
+                OA = correct_prediction.cpu() / available_label_count
+
+                return OA
+        else:
+            with torch.no_grad():
+
+                available_label_idx = (train_samples_gt != 0).float()
+                available_label_count = available_label_idx.sum()
+                correct_prediction = torch.where(
+                    torch.argmax(network_output, 1) == torch.argmax(train_samples_gt_onehot, 1), available_label_idx,
+                    zeros).sum()
+                OA = correct_prediction.cpu() / available_label_count
+                OA = OA.cpu().numpy()
+
+                # 计算AA
+                zero_vector = np.zeros([class_count])
+                output_data = network_output.cpu().numpy()
+                train_samples_gt = train_samples_gt.cpu().numpy()
+                train_samples_gt_onehot = train_samples_gt_onehot.cpu().numpy()
+
+                output_data = np.reshape(output_data, [m * n, class_count])
+                idx = np.argmax(output_data, axis=-1)
+                for z in range(output_data.shape[0]):
+                    if ~(zero_vector == output_data[z]).all():
+                        idx[z] += 1
+                # idx = idx + train_samples_gt
+                count_perclass = np.zeros([class_count])
+                correct_perclass = np.zeros([class_count])
+                for x in range(len(train_samples_gt)):
+                    if train_samples_gt[x] != 0:
+                        count_perclass[int(train_samples_gt[x] - 1)] += 1
+                        if train_samples_gt[x] == idx[x]:
+                            correct_perclass[int(train_samples_gt[x] - 1)] += 1
+                test_AC_list = correct_perclass / count_perclass
+                test_AA = np.average(test_AC_list)
+
+                # 计算KPP
+                test_pre_label_list = []
+                test_real_label_list = []
+                output_data = np.reshape(output_data, [m * n, class_count])
+                idx = np.argmax(output_data, axis=-1)
+                idx = np.reshape(idx, [m, n])
+                for ii in range(m):
+                    for jj in range(n):
+                        if Test_GT[ii][jj] != 0:
+                            test_pre_label_list.append(idx[ii][jj] + 1)
+                            test_real_label_list.append(Test_GT[ii][jj])
+                test_pre_label_list = np.array(test_pre_label_list)
+                test_real_label_list = np.array(test_real_label_list)
+                kappa = metrics.cohen_kappa_score(test_pre_label_list.astype(np.int16),
+                                                test_real_label_list.astype(np.int16))
+                test_kpp = kappa
+
+                if printFlag:
+                    print("test OA=", OA, "AA=", test_AA, 'kpp=', test_kpp)
+                    print('acc per class:')
+                    print(test_AC_list)
+
+                OA_ALL.append(OA)
+                AA_ALL.append(test_AA)
+                KPP_ALL.append(test_kpp)
+                AVG_ALL.append(test_AC_list)
+
+                # 保存数据信息
+                f = open('results\\' + dataset_name + '_results.txt', 'a+')
+                str_results = '\n======================' \
+                                + " iteration=" + str(curr_seed) \
+                                + " learning rate=" + str(learning_rate) \
+                                + " epochs=" + str(max_epoch) \
+                                + " train ratio=" + str(train_ratio) \
+                                + " val ratio=" + str(val_ratio) \
+                                + " ======================" \
+                                + "\nOA=" + str(OA) \
+                                + "\nAA=" + str(test_AA) \
+                                + '\nkpp=' + str(test_kpp) \
+                                + '\nacc per class:' + str(test_AC_list) + "\n"
+                # + '\ntrain time:' + str(time_train_end - time_train_start) \
+                # + '\ntest time:' + str(time_test_end - time_test_start) \
+                f.write(str_results)
+                f.close()
+                return OA
+
+
+    # 贪心训练策略参数
+    LOSS_LIST = []
+    # early_stop_nums = 9
+    # early_stop_nums = 7
+    early_stop_nums = 5
+    # early_stop_nums = 3
+    early_epoch = 0
+    early_flags_nums = 10
+    early_flags = 0
+    # 训练
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+    best_loss = 99999
+    best_OA = 0.5
+    net.train()
+    tic1 = time.perf_counter()
+    for i in range(max_epoch + 1):
+        query = []
+        optimizer.zero_grad()  # zero the gradient buffers
+        output = net(net_input)
+        loss = compute_loss(output, train_samples_gt_onehot, train_label_mask)
+        loss.backward(retain_graph=False)
+        optimizer.step()  # Does the update
+        with torch.no_grad():
+            net.eval()
+            output = net(net_input)
+
+            al = GreedyTrainingStrategy.GTS(output, train_data_index, val_data_index, test_data_index)
+
+            trainloss = compute_loss(output, train_samples_gt_onehot, train_label_mask)
+            trainOA = evaluate_performance(output, train_samples_gt, train_samples_gt_onehot)
+            valloss = compute_loss(output, val_samples_gt_onehot, val_label_mask)
+            valOA = evaluate_performance(output, val_samples_gt, val_samples_gt_onehot)
+            # print("{}\ttrain loss={}\t train OA={} val loss={}\t val OA={}".format(str(i + 1), trainloss, trainOA,
+            #                                                                         valloss, valOA))
+            print("Epoch={} \t train loss={}\t  train OA={}\t  val loss={}\t  val OA={}".format(str(i + 1),
+                    "%.4f" % trainloss, "%.2f%%" % (trainOA * 100), "%.4f" % valloss, "%.2f%%" % (valOA * 100)))
+            if valloss < best_loss:
+                best_loss = valloss
+                torch.save(net.state_dict(), "model\\best_model.pt")
+                # print('save model...')
+            LOSS_LIST.append(valloss)
+
+        torch.cuda.empty_cache()
+        net.train()
+
+        if early_flags != early_flags_nums and i >= 80:
+            if LOSS_LIST[-2] < LOSS_LIST[-1]:
+                print('curr hop:', early_epoch)
+                early_epoch += 1
+                LOSS_LIST[-1] = LOSS_LIST[-2]
+                print('curr hop+1:', early_epoch)
+                if early_epoch == early_stop_nums:
+                    print('trigger hop:', early_epoch)
+                    print('LOSS:', LOSS_LIST[-2])
+                    train_data_index, val_data_index, test_data_index = al.balanced_sample(dataset_name)
+
+                    train_samples_gt, test_samples_gt, val_samples_gt, \
+                        train_samples_gt_onehot, test_samples_gt_onehot, val_samples_gt_onehot, \
+                        train_label_mask, test_label_mask, val_label_mask \
+                        = al.sampling(height, width, class_count, test_data_index, train_data_index, val_data_index, gt, device)
+                    early_flags += 1
+                    early_epoch = 0
+            else:
+                early_epoch = 0
+
+    toc1 = time.perf_counter()
+    print("\n\n====================training done. starting evaluation...========================\n")
+    training_time = toc1 - tic1 + LDA_SLIC_Time
+    Train_Time_ALL.append(training_time)
+    day = datetime.datetime.now()
+    day_str = day.strftime('%m_%d_%H_%M')
+    torch.cuda.empty_cache()
+    with torch.no_grad():
+        net.load_state_dict(torch.load("model\\best_model.pt"))
+        net.eval()
+        tic2 = time.perf_counter()
+        output = net(net_input)
+        toc2 = time.perf_counter()
+
+        testloss = compute_loss(output, test_samples_gt_onehot, test_label_mask)
+        testOA = evaluate_performance(output, test_samples_gt, test_samples_gt_onehot, require_AA_KPP=True,
+                                        printFlag=False)
+        # print("{}\ttest loss={}\t test OA={}".format(str(i + 1), testloss, testOA))
+        print("\ttest loss={}\t test OA={}".format(testloss, testOA))
+
+        # 计算
+        testing_time = toc2 - tic2 + LDA_SLIC_Time
+        Test_Time_ALL.append(testing_time)
+
+    torch.cuda.empty_cache()
+    del net
+
+OA_ALL = np.array(OA_ALL)
+AA_ALL = np.array(AA_ALL)
+KPP_ALL = np.array(KPP_ALL)
+AVG_ALL = np.array(AVG_ALL)
+Train_Time_ALL = np.array(Train_Time_ALL)
+Test_Time_ALL = np.array(Test_Time_ALL)
+
+print("\ntrain_ratio={}".format(curr_train_ratio),
+        "\n==============================================================================")
+print('OA=', np.mean(OA_ALL), '+-', np.std(OA_ALL))
+print('AA=', np.mean(AA_ALL), '+-', np.std(AA_ALL))
+print('Kpp=', np.mean(KPP_ALL), '+-', np.std(KPP_ALL))
+print('AVG=', np.mean(AVG_ALL, 0), '+-', np.std(AVG_ALL, 0))
+print("Average training time:{}".format(np.mean(Train_Time_ALL)))
+print("Average testing time:{}".format(np.mean(Test_Time_ALL)))
+day = datetime.datetime.now()
+day_str = day.strftime('%m_%d_%H_%M')
+
+f = open('results\\' + dataset_name + '_results.txt', 'a+')
+str_results = '\n\n************************************************'+day_str +'************************************************'\
+                + "\ntrain_ratio={}".format(curr_train_ratio) \
+                + '\nOA=' + str(np.mean(OA_ALL)) + '+-' + str(np.std(OA_ALL)) \
+                + '\nAA=' + str(np.mean(AA_ALL)) + '+-' + str(np.std(AA_ALL)) \
+                + '\nKpp=' + str(np.mean(KPP_ALL)) + '+-' + str(np.std(KPP_ALL)) \
+                + '\nAVG=' + str(np.mean(AVG_ALL, 0)) + '+-' + str(np.std(AVG_ALL, 0)) \
+                + "\nAverage training time:{}".format(np.mean(Train_Time_ALL)) \
+                + "\nAverage testing time:{}".format(np.mean(Test_Time_ALL))
+f.write(str_results)
+f.close()
---
+++ Network.py
@@ -0,0 +1,304 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from einops import rearrange
+
+
+class GraphSelfAttentionLayer(nn.Module):
+    def __init__(self, in_features, out_features, adj, dropout, alpha, concat=True):
+        super(GraphSelfAttentionLayer, self).__init__()
+        self.dropout = dropout
+        self.alpha = alpha
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.concat = concat
+
+        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)), requires_grad=True)
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)
+
+        self.to_linear = nn.Linear(out_features, out_features)
+
+        self.a = nn.Parameter(torch.empty(size=(2 * out_features, 1)), requires_grad=True)
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+        self.to_q = nn.Linear(out_features, out_features*1)
+        self.to_kv = nn.Linear(out_features, out_features*2)
+        self.to_qkv = nn.Linear(out_features, out_features*3)
+
+        self.scale = out_features ** -0.5  # 1/sqrt(dim)
+        self.heads = 4
+        self.adj_bn = nn.BatchNorm2d(in_features)
+
+    def forward(self, h, adj):
+        # h.shape torch.Size([196, 128])  # 每个超像素包含一个长度为128的序列信息
+        Wh = torch.mm(h, self.W)  # h.shape: (N, in_features), Wh.shape: (N, out_features)
+        Wh = self.to_linear(Wh)
+        qkv = self.to_qkv(Wh).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'n (h d) -> h n d', h=self.heads), qkv)  # split into multi head attentions
+        dots = torch.einsum('h i d, h j d -> h i j', q, k) * self.scale
+
+        dots = F.softmax(dots, dim=-1)
+        Wh0 = torch.einsum('h i j, h j d -> h i d', dots, v)
+        Wh0 = rearrange(Wh0, 'h i d -> i (h d)')
+        Wh0 = F.softmax(Wh0, dim=-1)
+        a_input = self._prepare_attentional_mechanism_input(Wh)
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))  # 196 196 60 * 60 1  196 196 1
+        zero_vec = -9e15 * torch.ones_like(e)
+        dots = torch.sum(dots, dim=0)
+        attention = torch.where(adj > 0, e + dots, zero_vec)
+        attention = F.softmax(attention, dim=1)
+        attention = F.dropout(attention, self.dropout, training=self.training)
+        h_prime = torch.matmul(attention, Wh) + Wh0
+
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+    def _prepare_attentional_mechanism_input(self, Wh):
+        N = Wh.size()[0]  # number of nodes
+        Wh_repeated_in_chunks = Wh.repeat_interleave(N, dim=0)
+        Wh_repeated_alternating = Wh.repeat(N, 1)
+        all_combinations_matrix = torch.cat([Wh_repeated_in_chunks, Wh_repeated_alternating], dim=1)
+        return all_combinations_matrix.view(N, N, 2 * self.out_features)
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+
+
+# source from https://github.com/PetarV-/GAT
+class GAT(nn.Module):
+    def __init__(self, nfeat, nhid, adj, nout, dropout, alpha, nheads):
+        """Dense version of GAT."""
+        super(GAT, self).__init__()
+        self.dropout = dropout
+        self.adj = adj  # 邻接矩阵
+        self.attentions = [GraphSelfAttentionLayer(nfeat, nhid, adj, dropout=dropout, alpha=alpha, concat=True) for _ in
+                            range(nheads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)
+        self.out_att = GraphSelfAttentionLayer(nhid * nheads, nout, adj, dropout=dropout, alpha=alpha, concat=False)
+        self.norm = nn.LayerNorm(nhid * nheads)
+
+    def forward(self, x):
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(torch.cat([att(x, self.adj) for att in self.attentions], dim=1))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(self.out_att(x, self.adj))
+        return x
+
+
+class CAM_Module(nn.Module):
+    """ Channel attention module"""
+
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        m_batchsize, C, height, width = x.size()  # 1 128 145 145
+        proj_query = x.view(m_batchsize, C, -1)  # 1 128 145*145
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)  # 1 145*145 128
+        energy = torch.bmm(proj_query, proj_key)  # 1 128 128
+        energy_new1 = torch.max(energy, -1, keepdim=True)[0].expand_as(energy) - energy
+        attention = self.softmax(energy_new1)
+        proj_value = x.view(m_batchsize, C, -1)  # 1 128 145*145
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+        out = self.gamma * out + x
+        return out
+
+
+class SSConv(nn.Module):
+    '''
+    Spectral-Spatial Convolution
+    '''
+    def __init__(self, in_ch, out_ch, kernel_size=3):
+        super(SSConv, self).__init__()
+        self.point_conv = nn.Conv2d(
+            in_channels=in_ch,
+            out_channels=out_ch,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            groups=1,
+            bias=False
+        )
+        self.depth_conv = nn.Conv2d(
+            in_channels=out_ch,
+            out_channels=out_ch,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=kernel_size // 2,
+            groups=out_ch
+        )
+        self.Act1 = nn.LeakyReLU()
+        self.Act2 = nn.LeakyReLU()
+        self.BN = nn.BatchNorm2d(in_ch)
+
+    def forward(self, input):
+        out = self.BN(input)
+        out = self.point_conv(out)
+        out = self.Act1(out)
+
+        out = self.depth_conv(out)
+        out = self.Act2(out)
+        return out
+
+
+class SSConv1(nn.Module):
+    '''
+    Spectral-Spatial Convolution
+    '''
+    def __init__(self, in_ch, out_ch,kernel_size):
+        super(SSConv1, self).__init__()
+        self.depth_conv = nn.Conv2d(
+            in_channels=out_ch,
+            out_channels=out_ch,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=kernel_size//2,
+            groups=out_ch,
+            # dilation=2
+        )
+        self.point_conv = nn.Conv2d(
+            in_channels=in_ch,
+            out_channels=out_ch,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            groups=1,
+            bias=False
+        )
+        self.Act1 = nn.LeakyReLU()
+        self.Act2 = nn.LeakyReLU()
+        self.BN = nn.BatchNorm2d(in_ch)
+
+    def forward(self, input):
+        out = self.point_conv(self.BN(input))
+        out1 = self.Act1(out)
+        out = self.depth_conv(out1)
+        out = self.Act2(out)
+        return out
+
+
+class GS_GraphSAT(nn.Module):
+    def __init__(self, height: int, width: int, changel: int, class_count: int, Q: torch.Tensor, A: torch.Tensor,
+                 model='normal'):
+        super(GS_GraphSAT, self).__init__()
+        self.name = 'GS_GraphSAT'
+        self.class_count = class_count  # 类别数
+        self.channel = changel
+        self.height = height
+        self.width = width
+        self.Q = Q
+        self.A = A
+        self.model = model
+        self.norm_col_Q = Q / (torch.sum(Q, 0, keepdim=True))  # 列归一化 Q
+        self.WH = 0
+        self.M = 2
+
+        layers_count = 2
+
+        self.CNN_denoise = nn.Sequential()
+        for i in range(layers_count):
+            if i == 0:
+                self.CNN_denoise.add_module('CNN_denoise_BN' + str(i), nn.BatchNorm2d(self.channel))
+                self.CNN_denoise.add_module('CNN_denoise_Conv' + str(i), nn.Conv2d(self.channel, 128, kernel_size=1))
+                self.CNN_denoise.add_module('CNN_denoise_Act' + str(i), nn.LeakyReLU())
+            else:
+                self.CNN_denoise.add_module('CNN_denoise_BN' + str(i), nn.BatchNorm2d(128), )
+                self.CNN_denoise.add_module('CNN_denoise_Conv' + str(i), nn.Conv2d(128, 128, kernel_size=1))
+                self.CNN_denoise.add_module('CNN_denoise_Act' + str(i), nn.LeakyReLU())
+
+        self.CNN_Branch = nn.Sequential()
+        for i in range(layers_count):
+            if i < layers_count - 1:
+                self.CNN_Branch.add_module('Attention' + str(i), CAM_Module(128))
+                self.CNN_Branch.add_module('CNN_Branch' + str(i), SSConv(128, 128, kernel_size=3))
+                self.CNN_Branch.add_module('Drop_Branch'+str(i), nn.Dropout(0.2))
+            else:
+                self.CNN_Branch.add_module('Attention' + str(i), CAM_Module(128))
+                self.CNN_Branch.add_module('CNN_Branch' + str(i), SSConv(128, 64, kernel_size=5))
+
+        self.Attention = CAM_Module(128)
+
+        self.CNN_Branch11 = SSConv(128, 128, kernel_size=1)
+        self.CNN_Branch12 = SSConv(128, 64, kernel_size=3)
+        self.CNN_Branch21 = SSConv1(128, 128, kernel_size=5)
+        self.CNN_Branch22 = SSConv1(128, 64, kernel_size=7)
+
+        self.GAT_Branch = nn.Sequential()
+        self.GAT_Branch.add_module('GAT_Branch' + str(i),
+                                    GAT(nfeat=128, nhid=32, adj=A, nout=64, dropout=0.4, nheads=4, alpha=0.2))
+
+        self.linear1 = nn.Linear(64, 64)
+        torch.nn.init.kaiming_normal_(self.linear1.weight, mode='fan_in', nonlinearity='leaky_relu')
+        self.act1 = nn.LeakyReLU()
+        self.bn1 = nn.BatchNorm1d(64)
+
+        self.Softmax_linear = nn.Sequential(nn.Linear(64, self.class_count))
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        # 降维 - 升维：减少计算量
+        self.fc = nn.Sequential(
+            nn.Linear(128, 128 // 8, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(128 // 8, 128, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x: torch.Tensor):
+        (h, w, c) = x.shape  # 145 145 200
+        # 降维 145 145 128
+        noise = self.CNN_denoise(torch.unsqueeze(x.permute([2, 0, 1]), 0))  # 输入 1 200 145 145 输出 1 128 145 145
+        noise = torch.squeeze(noise, 0).permute([1, 2, 0])
+        clean_x = noise  # (145, 145, 128)
+        # 构造超像素与像素之间的映射
+        clean_x_flatten = clean_x.reshape([h * w, -1])  # 145*145 128
+        superpixels_flatten = torch.mm(self.norm_col_Q.t(), clean_x_flatten)  # (196, 128) =(196, 21025) * (21025, 128)
+        hx = clean_x
+
+        # MAF
+        hx = self.Attention(torch.unsqueeze(hx.permute([2, 0, 1]), 0))
+        CNN_result11 = self.CNN_Branch11(hx)
+        CNN_result21 = self.CNN_Branch21(hx)
+        CNN_result = CNN_result11 + CNN_result21
+        CNN_result = self.Attention(CNN_result)
+
+        CNN_result11_pool = self.avg_pool(CNN_result11).view(1, 128)
+        CNN_result11_excite = CNN_result11 * self.fc(CNN_result11_pool).view(1, 128, 1, 1)
+        CNN_result11 = CNN_result11 * CNN_result11_excite
+
+        CNN_result21_pool = self.avg_pool(CNN_result21).view(1, 128)
+        CNN_result21_excite = CNN_result21 * self.fc(CNN_result21_pool).view(1, 128, 1, 1)
+        CNN_result21 = CNN_result21 * CNN_result21_excite
+
+        CNN_result1 = CNN_result + CNN_result11
+        CNN_result2 = CNN_result + CNN_result21
+
+        CNN_result12 = self.CNN_Branch12(CNN_result1)
+        CNN_result22 = self.CNN_Branch22(CNN_result2)
+        CNN_result = CNN_result12 + CNN_result22
+        CNN_result = self.Attention(CNN_result)
+
+        CNN_result = CNN_result + CNN_result12 + CNN_result22
+        CNN_result = torch.squeeze(CNN_result, 0).permute([1, 2, 0]).reshape([h * w, -1])
+
+        # 图注意力
+        H = superpixels_flatten
+        H = self.GAT_Branch(H)  # 输出 196 64
+        GAT_result = torch.matmul(self.Q, H)  # (21025, 196) * (196, 64) = (21025, 64)
+        GAT_result = self.linear1(GAT_result)
+        GAT_result = self.act1(self.bn1(GAT_result))
+
+        Y = 0.05 * CNN_result + 0.95 * GAT_result
+        Y = self.Softmax_linear(Y)
+        Y = F.softmax(Y, -1)
+        return Y
